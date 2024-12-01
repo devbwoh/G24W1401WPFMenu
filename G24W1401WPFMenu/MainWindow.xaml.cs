b8441106f5f65a3553f26679f2eb9199910626e9
BWOH <bwoh@outlook.kr>
2024-12-02 05:04:30 +0900

    🚀 메뉴 완성

--- G24W1401WPFMenu/MainWindow.xaml.cs
+++ G24W1401WPFMenu/MainWindow.xaml.cs
@@ -19,6 +19,37 @@ namespace G24W1401WPFMenu {
 
             ItemWhite.IsEnabled = false;
             ItemWhite.IsChecked = true;
+
+            // WPF에서 제공하는 기본 명령 중 하나인 "열기" 바인딩 생성
+            CommandBinding bind = new CommandBinding(ApplicationCommands.Open);
+
+            // 명령이 실행될 때 호출될 이벤트 핸들러 지정
+            // 내가 만든 OpenDocument 메서드
+            bind.Executed += OpenDocument;
+
+            // CommandBindings: 현재 UI 요소에 대한 명령과 이벤트 바인딩 목록을 포함하는 컬렉션
+            // 앞서 생성한 bind를 CommandBindings 컬렉션에 추가하여 명령과 이벤트 연결
+            CommandBindings.Add(bind);
+        }
+
+        private void OpenDocument(object sender, RoutedEventArgs e) {
+            // Configure open file dialog box
+            var dialog = new Microsoft.Win32.OpenFileDialog();
+            dialog.FileName = "이미지"; // Default file name
+            dialog.DefaultExt = ".jpg"; // Default file extension
+            dialog.Filter = "Images (.jpg)|*.jpg"; // Filter files by extension
+
+            // Show open file dialog box
+            bool? result = dialog.ShowDialog();
+
+            // Process open file dialog box results
+            if (result != true)
+                return;
+
+            // Open document
+            string filename = dialog.FileName;
+
+            MessageBox.Show(filename);
         }
 
         private void SetRed(object sender, RoutedEventArgs e) {
